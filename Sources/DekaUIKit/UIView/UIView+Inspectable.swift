c402725bd1929680df6658b7c65299e4eedfb5da
Ginés Navarro <gines@dekalabs.com>
2020-11-02 12:23:37 +0100

    UIView IBInspectable Extensions

--- Sources/DekaUIKit/UIView/UIView+Inspectable.swift
+++ Sources/DekaUIKit/UIView/UIView+Inspectable.swift
@@ -18,4 +18,73 @@ extension UIView {
             layer.cornerRadius = newValue
         }
     }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor {
+        get {
+            guard let color = layer.borderColor else {
+                return .black
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            guard let color = layer.shadowColor else {
+                return .black
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffsetWidth: CGFloat {
+        get {
+            return layer.shadowOffset.width
+        }
+        set {
+            layer.shadowOffset = CGSize(width: newValue, height: layer.shadowOffset.height)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetHeight: CGFloat {
+        get {
+            return layer.shadowOffset.height
+        }
+        set {
+            layer.shadowOffset = CGSize(width: layer.shadowOffset.width, height: newValue)
+        }
+    }
 }
--- Tests/DekaUIKitTests/UIView/UIViewInspectableTests.swift
+++ Tests/DekaUIKitTests/UIView/UIViewInspectableTests.swift
@@ -3,11 +3,67 @@ import XCTest
 
 final class UIViewInspectableTests: XCTestCase {
     
-    func testUIViewCornerRadius() {
+    func testCornerRadius() {
         let cornerRadius = CGFloat(20)
         let view = UIView()
         view.cornerRadius = cornerRadius
         
         XCTAssertEqual(view.layer.cornerRadius, cornerRadius)
+    }
+    
+    func testBorderWidth() {
+        let borderWidth = CGFloat(4)
+        let view = UIView()
+        view.borderWidth = borderWidth
+        
+        XCTAssertEqual(view.layer.borderWidth, borderWidth)
+    }
+    
+    func testBorderColor() {
+        let borderColor: UIColor = .red
+        let view = UIView()
+        view.borderColor = borderColor
+        
+        XCTAssertEqual(view.layer.borderColor, borderColor.cgColor)
+    }
+    
+    func testShadowColor() {
+        let shadowColor: UIColor = .red
+        let view = UIView()
+        view.shadowColor = shadowColor
+        
+        XCTAssertEqual(view.layer.shadowColor, shadowColor.cgColor)
+    }
+    
+    func testShadowRadius() {
+        let shadowRadius = CGFloat(5)
+        let view = UIView()
+        view.shadowRadius = shadowRadius
+        
+        XCTAssertEqual(view.layer.shadowRadius, shadowRadius)
+    }
+    
+    func testShadowOpacity() {
+        let shadowOpacity = Float(1)
+        let view = UIView()
+        view.shadowOpacity = shadowOpacity
+        
+        XCTAssertEqual(view.layer.shadowOpacity, shadowOpacity)
+    }
+    
+    func testShadowOffsetWidth() {
+        let shadowOffset = CGFloat(2)
+        let view = UIView()
+        view.shadowOffsetWidth = shadowOffset
+        
+        XCTAssertEqual(view.layer.shadowOffset.width, shadowOffset)
+    }
+    
+    func testShadowOffsetHeight() {
+        let shadowOffset = CGFloat(3)
+        let view = UIView()
+        view.shadowOffsetHeight = shadowOffset
+        
+        XCTAssertEqual(view.layer.shadowOffset.height, shadowOffset)
     }
 }
