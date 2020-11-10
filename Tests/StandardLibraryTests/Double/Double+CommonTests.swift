f012b1b1f1d30d87fc6f6951fa03635b24673936
Ginés Navarro <gines@dekalabs.com>
2020-11-10 13:29:46 +0100

    Double extensions

---
+++ Tests/StandardLibraryTests/Double/Double+CommonTests.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ginés Navarro Fernández on 10/11/2020.
+//
+
+import XCTest
+@testable import StandardLibrary
+
+final class DoubleCommonTests: XCTestCase {
+    
+    func testValidToInt() {
+        let value = Double(12)
+        
+        XCTAssertEqual(value.toInt(),  12)
+    }
+}
