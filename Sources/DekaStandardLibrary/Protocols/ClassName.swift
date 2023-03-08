5c5e9e578399752f8a080a2a941766198ccf5457
Roberto Broseta Vigara <roberto@dekalabs.com>
2023-03-08 08:13:21 +0100

    fix: Fixed ClassName access level

--- Sources/DekaStandardLibrary/Protocols/ClassName.swift
+++ Sources/DekaStandardLibrary/Protocols/ClassName.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-protocol ClassName {
+public protocol ClassName {
     static var className: String { get }
 }
 
-extension ClassName {
+public extension ClassName {
     static var className: String {
         let name = String(describing: self)
         return name
