94301a801653c18f4cc5915c92332bcf9edfb20b
José Sancho Monrabal <jsancho@dekalabs.com>
2023-07-03 12:00:16 +0200

    Add localized method with arguments

--- Sources/DekaStandardLibrary/String/String+Localization.swift
+++ Sources/DekaStandardLibrary/String/String+Localization.swift
@@ -4,4 +4,8 @@ extension String {
     public func localized() -> String {
         return NSLocalizedString(self, comment: "")
     }
+
+    func localized(with args: CVarArg...) -> String {
+        return String(format: self.localized(), arguments: args)
+    }
 }
